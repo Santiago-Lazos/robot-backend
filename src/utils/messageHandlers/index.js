5574b20f3f0114de57b5ca042d67f2b9fba1d31d
Santiago-Lazos <santiago.daniel.lazos@gmail.com>
2025-10-24 17:47:44 -0300

    fix de index.js

--- src/utils/messageHandlers/index.js
+++ src/utils/messageHandlers/index.js
@@ -2,8 +2,9 @@ export { handleAck } from "./handleAck.js";
 export { handleConnected } from "./handleConnected.js";
 export { handleDisconnected } from "./handleDisconnected.js";
 export { handleError } from "./handleError.js";
+export { handleImage } from "./handleImage.js";
+export { handleObstacle } from "./handleObstacle.js";
+export { handleUnknown } from "./handleUnknown.js";
 
-// por si llega un tipo desconocido
-export function handleUnknown(body) {
-  console.log("⚠️ Tipo de mensaje desconocido recibido:", body);
-}
+
+
